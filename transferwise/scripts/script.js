1223248daa79539b87a6cc479ba53b896a9d7465
Mart von Laus <mart@laus.ru>
2014-11-29 15:46:10 +0200

    asdasd

--- transferwise/scripts/script.js
+++ transferwise/scripts/script.js
@@ -1,5 +1,28 @@
-$( document ).ready(function() {
-    $('.selectpicker').selectpicker();
+function sendRequest(){
+  console.log("sending")
+  var data = document.getElementById('userInput').value;
+  $.ajax({
+            url: "http://challenge.transferwise.com/?teamname=<InnovateWise>data={"+data+"}",
+            type: "GET",
+            data: {sort: how},
+            success: function(data){
+                    console.log(data)
+                    $("#res").html("")
+            
+                     for(var i=0;i<data.length; i++){
+            
+             
+             
+         }
+                }
+       });
+}
+
+$(document).ready(function() {
+  $('.selectpicker').selectpicker({
+    style: 'btn-info',
+    size: 4
+  });
 });
 
 
